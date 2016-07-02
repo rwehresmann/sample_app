ce08b80a2f1c7a5984e80fbe36fc3053e4633a79
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-07-02 01:40:55 +0000

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  get '/help', to: 'static_pages#help' #, as: 'helf' # 'as' update the _path name
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 end
