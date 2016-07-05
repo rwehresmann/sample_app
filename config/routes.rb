542c2646ac00a6367e1c0fa6db4cab01521b4202
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-07-05 21:11:52 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   resources :account_activations, only: [:edit]
+
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
