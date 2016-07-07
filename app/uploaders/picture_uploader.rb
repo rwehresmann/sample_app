4d7be27de5bbda6d35999eb03ea02ea6af2c6ba3
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2016-07-07 12:42:55 +0000

    Allow file storage in production

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -13,11 +13,13 @@ class PictureUploader < CarrierWave::Uploader::Base
   end
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  #if Rails.env.production?
+  #  storage :fog
+  #else
+  #  storage :file
+  #end
+
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
